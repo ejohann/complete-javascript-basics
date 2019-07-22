fbb27ed41c0e89196261f3aa30476265478547f6
Johanne <johanne_lewis@hotmail.com>
2019-07-22 14:41:17 +0100

    Variable mutation and coercion

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@
  * Variables and Data Types
 */
 
+/*
 var firstName = 'Johanne';
 console.log(firstName);
 
@@ -19,3 +20,26 @@ console.log(job);
 //variable naming rules
 var _3years = 3;
 var johnMark = 'John and Mark';
+*/
+
+/*************************************
+ * Variable mutation and type coercion
+*/
+
+var firstName = 'Johanne';
+var age = 38;
+
+//type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'Developer';
+isMarried = false;
+
+console.log(firstName + ' is a '  + age + ' year old ' + job +'. Is he married? ' + isMarried);
+
+//variable mutation
+age = 'Thirty Eight';
+job = 'Disc Jockey';
+
+alert(firstName + ' is a '  + age + ' year old ' + job +'. Is he married? ' + isMarried);
