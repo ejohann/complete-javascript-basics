5bd2ec99dbd0f4ed9c3ae648832ab57cadff5364
Johanne <johanne_lewis@hotmail.com>
2019-07-23 08:49:31 +0100

    Boolean logic - OR

--- scripts.js
+++ scripts.js
@@ -214,13 +214,16 @@ else
 
 
 var firstName = 'Johanne';
-var age = 16;
+var age = 20;
 
 if(age < 13){
     console.log(firstName + ' is a boy');
 }
 else if(age > 12 && age < 20){
   console.log(firstName + ' is a teenager');        
+}
+else if(age >= 20 || age < 30){
+   console.log(firstName + ' is a young man');     
 }
 else{
      console.log(firstName + ' is a man');
