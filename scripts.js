55f159e7026e6a4e96e2be856e8efe5cc7acc9a9
Johanne <johanne_lewis@hotmail.com>
2019-07-23 08:44:41 +0100

    Boolean logic - AND

--- scripts.js
+++ scripts.js
@@ -162,7 +162,7 @@ console.log('Is Mark\'s BMI higher than John\'s? ' + isBMIHigher);
 /*************************************
  * If / else statements
 */
-
+/*
 var firstName = 'Johanne';
 var civilStatus = 'single';
 
@@ -204,6 +204,57 @@ else
  {
      console.log('John\'s BMI is higher than Mark\'s');
  }
+*/
+
+
+
+/*************************************
+ * Boolean Logic
+*/
+
+
+var firstName = 'Johanne';
+var age = 16;
+
+if(age < 13){
+    console.log(firstName + ' is a boy');
+}
+else if(age > 12 && age < 20){
+  console.log(firstName + ' is a teenager');        
+}
+else{
+     console.log(firstName + ' is a man');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
