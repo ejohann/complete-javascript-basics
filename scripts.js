09dce3168e878c6ddef88f69d2a24b2e1e24fe45
Johanne <johanne_lewis@hotmail.com>
2019-07-23 16:52:35 +0100

    Coding challenge 2 lecturer solution

--- scripts.js
+++ scripts.js
@@ -396,9 +396,38 @@ switch(true){
 }
 
 
+// LECTURER SOLUTION
 
+var scoreJohn = (110 + 120 + 103) /3;
+var scoreMike = (116 + 97 + 123) /3;
+var scoreMary = (97 + 134 + 105) /3;
+console.log(scoreJohn, scoreMike, scoreMary);
 
+if(scoreJohn > scoreMike && scoreJohn > scoreMary){
+      console.log('John\'s team wins with ' + scoreJohn + ' points');
+  }
+else if(scoreMike > scoreJohn && scoreMike > scoreMary){
+      console.log('Mike\s team wins with ' + scoreMike + ' points'); 
+  }
+else if(scoreMary > scoreJohn && scoreMary > scoreMike){
+      console.log('Mary\s team wins with ' + scoreMary + ' points');   
+  }
+else {
+    console.log('There is a draw');
+}
 
+/*
+if(scoreJohn > scoreMike){
+    console.log('John\'s team wins with ' + scoreJohn + ' points');
+ }
+else if(scoreMike > scoreJohn){
+    console.log('Mike\s team wins with ' + scoreMike + ' points');   
+ }
+else{
+    console.log('There is a draw');   
+ }
+
+*/
 
 
 
