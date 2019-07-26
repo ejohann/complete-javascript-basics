e395078433dd7823bf94ee182a003e9b192b7ad9
Johanne <johanne_lewis@hotmail.com>
2019-07-26 16:54:37 +0100

    Lecturer's solution

--- scripts.js
+++ scripts.js
@@ -858,6 +858,7 @@ for(var i = john.length - 1; i >= 0; i--){
     
 */
 
+/*
  var john = {
      bills: [124, 48, 268, 180, 42],
      tips: [],
@@ -939,14 +940,96 @@ else
        console.log('Both families paid the same average tips: ' + johnTips, markTips);
    }
 
+*/
 
 
+//lecturer's solution
+
+var john = {
+    fullName: 'John Smith',
+    bills: [124, 48, 268, 100, 42],
+    calcTips: function(){
+        this.tips = [];
+        this.finalValues = [];
+        
+        for(var i = 0; i < this.bills.length; i++)
+          {
+              var percentage;
+              var bill = this.bills[i];
+              if(bill < 50)
+                  {
+                      percentage = .2;
+                  }
+              else if(bill >= 50 && bill < 200)
+                  {
+                      percentage = .15;
+                  }
+              else
+                  {
+                      percentage = .1;
+                  }
+              
+              this.tips[i] = bill * percentage;
+              this.finalValues[i] = bill + bill * percentage;
+          }
+    }
+}
 
 
+var mark = {
+    fullName: 'Mark Miller',
+    bills: [77, 475, 110, 45],
+    calcTips: function(){
+        this.tips = [];
+        this.finalValues = [];
+        
+        for(var i = 0; i < this.bills.length; i++)
+          {
+              var percentage;
+              var bill = this.bills[i];
+              if(bill < 100)
+                  {
+                      percentage = .2;
+                  }
+              else if(bill >= 100 && bill < 300)
+                  {
+                      percentage = .1;
+                  }
+              else
+                  {
+                      percentage = .25;
+                  }
+              
+              this.tips[i] = bill * percentage;
+              this.finalValues[i] = bill + bill * percentage;
+          }
+    }
+}
+
+function calcAverage(tips){
+    var sum = 0;
+    for(var i = 0; i < tips.length; i++)
+        {
+            sum = sum + tips[i];
+        }
+    return sum / tips.length;
+}
 
 
+john.calcTips();
+mark.calcTips();
 
+john.average = calcAverage(john.tips);
+mark.average = calcAverage(mark.tips);
 
+console.log(john, mark);
+
+if(john.average > mark.average){
+    console.log(john.fullName + '\'s family pays higher tips, with an average of $' + john.average);
+}
+else if(mark.average > john.average){
+   console.log(mark.fullName + '\'s family pays higher tips, with an average of $' + mark.average); 
+}
 
 
 
